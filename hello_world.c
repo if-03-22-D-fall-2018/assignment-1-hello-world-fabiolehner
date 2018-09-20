fa42bef45d45303d780ee755d46eed70dfb4f63a
Fabio Lehner <FabioLehner@Fabios-MacBook-Pro.local>
2018-09-20 11:34:58 +0200

    Block header wurde hinzugefügt

--- hello_world.c
+++ hello_world.c
@@ -1,3 +1,14 @@
+/*----------------------------------------------------------
+ *				HTBLA-Leonding / Class: 2DHIF
+ * ---------------------------------------------------------
+ * Exercise Number: 0
+ * Title:			Hello World
+ * Author:			Fabio Lehner
+ * ----------------------------------------------------------
+ * Description:
+ * Prints Hello World in the Console
+ * ----------------------------------------------------------
+ */
 #include <stdio.h>
 
 int main(int argc, char const *argv[])
